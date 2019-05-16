8d8d0c724852761c190c315cc49d7c4cc084354b
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-16 06:14:12 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,19 @@
+require 'pry'
 class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
+
+  def full_name
+    name = "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    arrays = []
+    role = self.characters.collect{|c|c.name}
+    show = self.shows.collect {|s|s.name}
+    combine = role.concat show
+    arrays << combine.join(" - ")
+    # binding.pry
+    arrays
+  end
 end
